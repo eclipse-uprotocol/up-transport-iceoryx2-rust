baea05a831a287504972dda31168cfd042411f43
Kenneth Mead <kennethmead784@gmail.com>
2025-08-23 19:49:36 +0000

    added copyright to nearly everything

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+// ################################################################################
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+// 
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+// 
+// This program and the accompanying materials are made available under the
+// terms of the Apache License Version 2.0 which is available at
+// https: //www.apache.org/licenses/LICENSE-2.0
+// 
+// SPDX-License-Identifier: Apache-2.0
+// ################################################################################
+
 use async_trait::async_trait;
 use std::sync::Arc;
 use up_rust::{UCode, UListener, UMessage, UStatus, UTransport, UUri};
@@ -22,18 +35,18 @@ impl UTransport for Iceoryx2Transport {
 
     async fn register_listener(
         &self,
-        source_filter: &UUri,
-        sink_filter: Option<&UUri>,
-        listener: Arc<dyn UListener>,
+        _source_filter: &UUri,
+        _sink_filter: Option<&UUri>,
+        _listener: Arc<dyn UListener>,
     ) -> Result<(), UStatus> {
         todo!()
     }
 
     async fn unregister_listener(
         &self,
-        source_filter: &UUri,
-        sink_filter: Option<&UUri>,
-        listener: Arc<dyn UListener>,
+        _source_filter: &UUri,
+        _sink_filter: Option<&UUri>,
+        _listener: Arc<dyn UListener>,
     ) -> Result<(), UStatus> {
         todo!()
     }
